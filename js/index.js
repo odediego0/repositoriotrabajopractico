7dc4d4d1ab00070ba262e9645a9c5da6ebc69c65
odediego0 <odediego@udesa.edu.ar>
2025-11-14 12:11:24 -0300

    category

--- js/index.js
+++ js/index.js
@@ -1,2 +1,13 @@
-const busqueda = document.querySelector(".search-container");
-
+let lista = document.querySelector('.listaCategoria')
+fetch(`https://dummyjson.com/products/categories`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        for (let i = 0; i < data.length; i++) {
+            lista.innerHTML += `<li><a href="./category.html">${data[i].name}</a></li>`
+        }
+    })
+    .catch(function (error) {
+        console.log('Error: ' + error);
+    });
--- js/search-results.js
+++ js/search-results.js
@@ -1,17 +1,37 @@
-const busqueda = document.querySelector(".search-container input");
+let lista = document.querySelector('.listaCategoria')
+fetch(`https://dummyjson.com/products/categories`)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        for (let i = 0; i < data.length; i++) {
+            lista.innerHTML += `<li><a href="./category.html">${data[i].name}</a></li>`
+        }
+    })
+    .catch(function (error) {
+        console.log('Error: ' + error);
+    });
 
-if (busqueda == '') {
-    alert("La búsqueda no puede estar vacía");
-} else if (busqueda < 3) {
-    alert("La búsqueda debe tener al menos 3 caracteres");
-}
+const form = document.querySelector(".search-container");
+const busqueda = document.querySelector(".buscar");
+
+form.addEventListener('submit', function (event){
+    event.preventDefault();
+    if (busqueda.value == '' ) {
+        alert('Por favor este campo no debe estar vacio');
+       
+    } else if (busqueda.value.length < 3 ) {
+        alert('Por favor ingrese al menos 3 caracteres');
+    } else {
+        this.submit();
+    }
+})
 
 const querystring = location.search;
 const querystringobj = new URLSearchParams(querystring);
 const buscador = querystringobj.get('q');
 
 console.log(buscador);
-
 
 const resultado = document.querySelector('.category-title');
 resultado.innerHTML = 'Resultados de búsqueda para ' + buscador;
