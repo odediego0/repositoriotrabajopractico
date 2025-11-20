05c4fed1626e1eb859012af25d56c400ef04c745
ibrauni <ibraun@udesa.edu.ar>
2025-11-20 20:13:37 -0300

    register

--- js/register.js
+++ js/register.js
@@ -11,3 +11,17 @@ form.addEventListener('submit', function (event){
         this.submit();
     }
 })
+
+let input_emailr = document.querySelector(".emailr");
+let input_passwordr = document.querySelector(".passwordr");
+let input_passwordr2 = document.querySelector(".passwordrr");
+let chek_terminos = document.querySelector("#chekbox");
+
+form.addEventListener('submit', function (t){
+    t.preventDefault();
+    if (input_passwordr.value() == '') {
+        alert('El campo contraseña no debe estar vacío');
+    } else if (input_passwordr.value.length < 6) {
+        alert('La contraseña debe tener al menos 6 caracteres');
+    }   
+    })
